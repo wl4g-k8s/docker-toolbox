d790d3d7418cb9d2d8b856f7f3f379d6481f1f73
James Wong <jameswong1376@gmail.com>
2023-11-29 22:56:16 +0800

    fix: mat build

--- mat/README.md
+++ mat/README.md
@@ -1,12 +1,15 @@
-# What is toolbox-mat?
+# What is toolbox-mat ?
+
 Toolbox-mat is a docker container that allows you to analyse a Java heap dump from the commandline without any GUI involved. You can get html reports directly from the commandline.
 
-# How to run?
+# How to run ?
+
 All you need is Docker installed on your machine.
 
 cd to the location of your heap dump file, then:
+
 ```bash
-docker run --mount src=$(pwd),target=/data,type=bind -it wl4g/toolbox-mat <dump filename> <heap size for mat> <reports>
+docker run --rm -v $(pwd):/data -it wl4g/toolbox-mat <dump filename> <heap size for mat> <reports>
 ```
 
 dump filename - hprof heap dump file name
@@ -15,27 +18,26 @@ heap size for mat - how much memory to consume for this process e.g (5g)
 
 reports - a comma separated list of the following: suspects, overview, top_components. If empty no reports will be generated. You probably want the suspects.
 
-
-### Example:
+### Example
 
 ```bash
-docker run -it --mount src=$(pwd),target=/data,type=bind wl4g/toolbox-mat heap1.hprof 8g suspects,overview
+docker run --rm -v $(pwd):/data wl4g/toolbox-mat heap1.hprof 8g suspects,overview
 ```
 
-# What problem does it solve?
+# What problem does it solve ?
 
 Analyzing large heap dumps is a long process which needs a lot of computing resources. Eclipse Memory Analyzer Tool (http://www.eclipse.org/mat/downloads.php) is GUI based tool to analyze heap dumps. It takes a lot of time to run an analysis for the first time with MAT and it consumes a lot of compute power, however when running for the second time it is fast, thats because MAT keeps indexes locally on the running machine.
 
 Toolbox-mat can help with that because it can let you run the analysis on any remote machine that has docker installed. Then you can copy the output to your local machine, run MAT and open the heap dump.
 Toolbox-mat will also generate HTML reports which might save you the effort of running MAT.
 
-# How to build:
+# How to build
 
 ```bash
 docker build . -t wl4g/toolbox-mat:<tag name>
 ```
 
-# How it works?
+# How it works ?
 
 This is a simple docker wrapper for tools that came from eclipse MAT. Eclipse MAT exposes command line tool to generate indexes and reports.
 
@@ -48,5 +50,3 @@ ubuntu:18.04 docker image https://hub.docker.com/_/ubuntu
 eclipse mat project https://www.eclipse.org/mat/
 
 Open jdk https://openjdk.java.net/
-
-
--- run.sh
+++ run.sh
@@ -20,71 +20,96 @@ export BASE_DIR=$(cd "`dirname $0`"/; pwd)
 function print_help() {
   echo $"
 Usage: ./$(basename $0) [OPTIONS] [arg1] [arg2] ...
-    build                                    Build of toolbox modules image.
-    push                                     Push the toolbox modules image to repoistory.
-          <repo_uri>                           Push to repoistory uri. format: <registryUri>/<namespace>, default: docker.io/wl4g
-                                             for example: registry.cn-shenzhen.aliyuncs.com/wl4g-k8s)
+    build                                    Build toolbox images.
+          all                                Build all toolbox images.
+          base                               Build for toolbox-base images.
+          arthas                             Build for toolbox-arthas images.
+          pprof                              Build for toolbox-pprof images.
+          mat                                Build for toolbox-mat image
+    push                                     Push toolbox images to remote repoistory.
+          all                                Push all toolbox images.
+          base                               Push for toolbox-base images.
+          arthas                             Push for toolbox-arthas images.
+          pprof                              Push for toolbox-pprof images.
+          mat                                Push for toolbox-mat image
+                <repo_uri>                   Push to repoistory uri. format: <registryUri>/<namespace>, default: docker.io/wl4g
+                                                for example: registry.cn-shenzhen.aliyuncs.com/wl4g-k8s)
 "
 }
 
 function build_images() {
+  local build_modules=$1
   local build_version=$(git branch | grep '*' | sed -E 's/\* \(HEAD detached at |\)|\* //g')
 
-  echo "Building for toolbox-base and toolbox-arthas ..."
-  cd $BASE_DIR/base && docker build -t wl4g/toolbox-base:${build_version} . &
-  cd $BASE_DIR/arthas && docker build -t wl4g/toolbox-arthas:${build_version} . &
-
-  echo "Building for toolbox-pprof ..."
-  if [ ! -d $BASE_DIR/pprof/gperftools ]; then
-    echo "No found precondidtions depends and cloning from: https://github.com/gperftools/gperftools"
-    cd $BASE_DIR/pprof && git clone https://github.com/gperftools/gperftools
+  if [[ $build_modules == *"base"* || $build_modules == *"all"* ]]; then
+    echo "Building for toolbox-base ..."
+    cd $BASE_DIR/base && docker build -t wl4g/toolbox-base:${build_version} . &
   fi
-  cd $BASE_DIR/pprof && docker build -t wl4g/toolbox-pprof:minideb-buster-${build_version} . &
 
-  echo "Building for toolbox-mat ..."
-  # see:https://eclipse.dev/mat/downloads.php
-  if [ ! -d $BASE_DIR/mat/mat ]; then
-    echo "No found precondidtions depends and downloading from: https://eclipse.dev/mat/downloads.php"
-    sudo apt install -y unzip
-    # china network to see: https://mirrors.neusoft.edu.cn/eclipse/mat/1.14.0/rcp/MemoryAnalyzer-1.14.0.20230315-linux.gtk.x86_64.zip
-    cd $BASE_DIR/mat && \
-curl -kL -o mat.zip 'https://mirror.umd.edu/eclipse/mat/1.14.0/rcp/MemoryAnalyzer-1.14.0.20230315-linux.gtk.x86_64.zip' \
-&& unzip mat.zip && rm -rf mat.zip # unzip -d ./mat mat.zip
+  if [[ $build_modules == *"arthas"* || $build_modules == *"all"* ]]; then
+    echo "Building for toolbox-arthas ..." 
+    cd $BASE_DIR/arthas && docker build -t wl4g/toolbox-arthas:${build_version} . &
   fi
-  cd $BASE_DIR/mat && docker build -t wl4g/toolbox-mat:${build_version} . &
+
+  if [[ $build_modules == *"pprof"* || $build_modules == *"all"* ]]; then
+    echo "Building for toolbox-pprof ..."
+    if [ ! -d $BASE_DIR/pprof/gperftools ]; then
+      echo "No found precondidtions depends and cloning from: https://github.com/gperftools/gperftools"
+      cd $BASE_DIR/pprof && git clone https://github.com/gperftools/gperftools
+    fi
+    cd $BASE_DIR/pprof && docker build -t wl4g/toolbox-pprof:minideb-buster-${build_version} . &
+  fi
+
+  if [[ $build_modules == *"mat"* || $build_modules == *"all"* ]]; then
+    echo "Building for toolbox-mat ..."
+    # see:https://eclipse.dev/mat/downloads.php
+    if [ ! -d $BASE_DIR/mat/mat ]; then
+      echo "No found precondidtions depends and downloading from: https://eclipse.dev/mat/downloads.php"
+      sudo apt install -y unzip
+      # china network to see: https://mirrors.neusoft.edu.cn/eclipse/mat/1.14.0/rcp/MemoryAnalyzer-1.14.0.20230315-linux.gtk.x86_64.zip
+      local dl_mat_url='https://mirror.umd.edu/eclipse/mat/1.14.0/rcp/MemoryAnalyzer-1.14.0.20230315-linux.gtk.x86_64.zip'
+      cd $BASE_DIR/mat && curl -kL -o mat.zip $dl_mat_url && unzip mat.zip && rm -rf mat.zip # unzip -d ./mat mat.zip
+    fi
+    cd $BASE_DIR/mat && docker build -t wl4g/toolbox-mat:${build_version} . &
+  fi
 
   wait
 }
 
 function push_images() {
+  local push_modules=$1
   local build_version=$(git branch | grep '*' | sed -E 's/\* \(HEAD detached at |\)|\* //g')
   local repo_uri="$1"
+
   [ -z "$repo_uri" ] && repo_uri="docker.io/wl4g"
   ## FIX: Clean up suffix delimiters for normalization '/'
   repo_uri="$(echo $repo_uri | sed -E 's|/$||g')"
 
-  echo "Tagging images to $repo_uri ..."
-  docker tag wl4g/toolbox-base:${build_version} $repo_uri/toolbox-base:${build_version}
-  docker tag wl4g/toolbox-arthas:${build_version} $repo_uri/toolbox-arthas:${build_version}
-  docker tag wl4g/toolbox-pprof:minideb-buster-${build_version} $repo_uri/toolbox-pprof:minideb-buster-${build_version}
-  docker tag wl4g/toolbox-mat:${build_version} $repo_uri/toolbox-mat:${build_version}
-
-  docker tag wl4g/toolbox-base:${build_version} $repo_uri/toolbox-base:latest
-  docker tag wl4g/toolbox-arthas:${build_version} $repo_uri/toolbox-arthas:latest
-  docker tag wl4g/toolbox-pprof:minideb-buster-${build_version} $repo_uri/toolbox-pprof:minideb-buster
-  docker tag wl4g/toolbox-mat:${build_version} $repo_uri/toolbox-mat:latest
-
-  echo "Pushing images of ${build_version}@$repo_uri ..."
-  docker push $repo_uri/toolbox-base:${build_version} &
-  docker push $repo_uri/toolbox-arthas:${build_version} &
-  docker push $repo_uri/toolbox-pprof:minideb-buster-${build_version} &
-  docker push $repo_uri/toolbox-mat:${build_version} &
-
-  echo "Pushing images of latest@$repo_uri ..."
-  docker push $repo_uri/toolbox-base &
-  docker push $repo_uri/toolbox-arthas &
-  docker push $repo_uri/toolbox-pprof:minideb-buster &
-  docker push $repo_uri/toolbox-mat &
+  if [[ $push_modules == *"base"* || $push_modules == *"all"* ]]; then
+    docker tag wl4g/toolbox-base:${build_version} $repo_uri/toolbox-base:${build_version}
+    docker tag wl4g/toolbox-base:${build_version} $repo_uri/toolbox-base:latest
+    echo "Pushing images of ${build_version}@$repo_uri ..."
+    docker push $repo_uri/toolbox-base:${build_version} &
+    docker push $repo_uri/toolbox-base &
+  fi
+  if [[ $push_modules == *"arthas"* || $push_modules == *"all"* ]]; then
+    docker tag wl4g/toolbox-arthas:${build_version} $repo_uri/toolbox-arthas:${build_version}
+    docker tag wl4g/toolbox-arthas:${build_version} $repo_uri/toolbox-arthas:latest
+    docker push $repo_uri/toolbox-arthas:${build_version} &
+    docker push $repo_uri/toolbox-arthas &
+  fi
+  if [[ $push_modules == *"pprof"* || $push_modules == *"all"* ]]; then
+    docker tag wl4g/toolbox-pprof:minideb-buster-${build_version} $repo_uri/toolbox-pprof:minideb-buster-${build_version}
+    docker tag wl4g/toolbox-pprof:minideb-buster-${build_version} $repo_uri/toolbox-pprof:minideb-buster
+    docker push $repo_uri/toolbox-pprof:minideb-buster-${build_version} &
+    docker push $repo_uri/toolbox-pprof:minideb-buster &
+  fi
+  if [[ $push_modules == *"mat"* || $push_modules == *"all"* ]]; then
+    docker tag wl4g/toolbox-mat:${build_version} $repo_uri/toolbox-mat:${build_version}
+    docker tag wl4g/toolbox-mat:${build_version} $repo_uri/toolbox-mat:latest
+    docker push $repo_uri/toolbox-mat:${build_version} &
+    docker push $repo_uri/toolbox-mat &
+  fi
 
   wait
 }
@@ -92,10 +117,48 @@ function push_images() {
 # --- Main. ---
 case $1 in
   build)
-    build_images
+    case $2 in
+        all)
+            build_images "all"
+            ;;
+        base)
+            build_images "base"
+            ;;
+        arthas)
+            build_images "arthas"
+            ;;
+        pprof)
+            build_images "pprof"
+            ;;
+        mat)
+            build_images "mat"
+            ;;
+        *)
+            print_help
+            ;;
+    esac
     ;;
   push)
-    push_images "$2"
+    case $2 in
+        all)
+            push_images "all" "$3"
+            ;;
+        base)
+            push_images "base" "$3"
+            ;;
+        arthas)
+            push_images "arthas" "$3"
+            ;;
+        pprof)
+            push_images "pprof" "$3"
+            ;;
+        mat)
+            push_images "mat" "$3"
+            ;;
+        *)
+            print_help
+            ;;
+    esac
     ;;
   *)
     print_help
